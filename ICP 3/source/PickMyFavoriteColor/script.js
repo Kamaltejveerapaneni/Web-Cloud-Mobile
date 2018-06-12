6c9ed5eb01b4b4ef9a94250784d12209d3d019f0
Kamal Tej <31327193+Kamaltejveerapaneni@users.noreply.github.com>
2018-06-12 16:53:28 -0500

    ICP 3
    Commit

---
+++ ICP 3/source/PickMyFavoriteColor/script.js
@@ -0,0 +1,48 @@
+var colors = [ "22ac5e", "d68236", "71b5c2", "af2655", "b34de7", "e6bd01", "104393", "ca4d94"];
+var favoriteColors = [];
+function setPreviewColor(color) {
+    $('.preview').css('background-color', color);
+    $('.color-code').text($('.preview').css('background-color'));
+}
+function addBox(color) {
+    $('#colors').prepend("<div class='item' style='background-color: " + color + ";'><div>");
+}
+$(document).ready(function(){
+
+
+    $.each( colors, function( i, val ) {
+
+        addBox(val);
+    });
+
+
+    setPreviewColor(colors[Math.floor(Math.random()*colors.length)]);
+    $(document).on('keydown keyup keypress', '#color', function(){
+        color = $(this).val();
+        setPreviewColor(color);
+    })
+
+    //adding to fav-color
+    $( "#add-to-favorite" ).click(function() {
+        color = $(this).prev().val();
+
+        $(this).prev().val("");
+        $(this).prev().focus();
+        if(favoriteColors.indexOf(color) <= -1){
+            favoriteColors.push(color);
+            addBox(color);
+        }
+        else
+            {
+            alert("color "+color+ " is already added to your favorites.")
+        }
+    });
+
+   //on hover
+    $(document).on('mouseover', '.item', function(){
+        color = $(this).css("background-color");
+        setPreviewColor(color);
+    })
+
+
+});
